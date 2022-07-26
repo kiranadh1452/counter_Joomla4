624a9bb543585deb4bcef972115889a601f0b73b
Kiran Adhikari <kiranadh1452@gmail.com>
2022-07-26 14:44:05 +0545

    Folder Structure + Install Setup Complete

---
+++ admin/src/View/Counter/HtmlView.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace KiranAdhikari\Component\Counter\Administrator\View\Counter;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_counter
+ *
+ * @copyright   Copyright (C) 2022 Kiran Adhikari. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Main "Counter" Admin View
+ */
+class HtmlView extends BaseHtmlView {
+    
+    /**
+     * Display the main "Counter" view
+     *
+     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+     * @return  void
+     */
+    function display($tpl = null) {
+        parent::display($tpl);
+    }
+
+
+}
