0c647019633c14ff77014c3120423044867a26c5
Kiran Adhikari <kiranadh1452@gmail.com>
2022-07-29 11:54:43 +0545

    Added a request variable to menu link

--- site/src/Model/MessageModel.php
+++ site/src/Model/MessageModel.php
@@ -4,8 +4,9 @@ namespace KiranAdhikari\Component\Counter\Site\Model;
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\MVC\Model\ItemModel;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\ItemModel;
 
 /**
  * @package     Joomla.Site
@@ -15,21 +16,37 @@ use Joomla\CMS\Language\Text;
  * @license     GNU General Public License version 3; see LICENSE
  */
 
-
 /**
  * Message Model
  */
-class MessageModel extends ItemModel {
+class MessageModel extends ItemModel
+{
 
-  /**
-   * Returns a message for display
-   * @param integer $pk Primary key of the "message item", currently unused
-   * @return object Message object
-   */
-  public function getItem($pk= null): object {
-      $item = new \stdClass();
-      $item->message = Text::_('COM_COUNTER_MSG_GREETING');
-      return $item;
-  }
-      
+    /**
+     * Returns a message for display
+     * @param integer $pk Primary key of the "message item", currently unused
+     * @return object Message object
+     */
+    public function getItem($pk = null): object
+    {
+        $input = Factory::getApplication()->getInput();
+        $greetingType = $input->getInt('greetingType', 1);
+
+        $item = new \stdClass();
+        // $item->message = Text::_('COM_COUNTER_MSG_GREETING');
+
+        switch ($greetingType) {
+            case 2:
+                $item->message = Text::_('COM_COUNTER_GREETING_TYPE_BYE_MSG');
+                break;
+
+            case 1:
+            default:
+                $item->message = Text::_('COM_COUNTER_GREETING_TYPE_WELCOME_MSG');
+                break;
+        }
+
+        return $item;
+    }
+
 }
